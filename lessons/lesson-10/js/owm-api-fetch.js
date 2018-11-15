17302fbbe45bdf81ca4f7a9287fb3662b2fb6e9b
Jason Blazzard <blazzard@gmail.com>
2018-11-15 09:39:27 -0700

    major weather api updates

--- lessons/lesson-10/js/owm-api-fetch.js
+++ lessons/lesson-10/js/owm-api-fetch.js
@@ -1,10 +1,17 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=07407eccd051a7a7b4fc81e187f47771')
+fetch('https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperial&APPID=07407eccd051a7a7b4fc81e187f47771')
     .then(function (response) {
         return response.json();
     })
-    .then(function (jsObj) {
-        console.log(jsObj);
-        document.getElementById('current-temp').innerHTML = jsObj.main.temp;
-        let imagesrc = 'http://openweathermap.org/img/w/' + jsObj.weather[0].icon + '.png';
+    .then(function (jsObject) {
+        
+        console.log(jsObject);
+
+        document.getElementById('current-temp').innerHTML = jsObject.main.temp;
+        document.getElementById('rain').innerHTML = jsObject.rain;
+        
+        let imagesrc = 'http://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        let desc = jsObject.weather[0].description;
         document.getElementById('weatherimage').innerHTML = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
     });
