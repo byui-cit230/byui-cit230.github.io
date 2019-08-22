bc7b40470d9cd7dd64caa947946a5d8bf97fb226
Blazzard <blazzard@gmail.com>
2019-08-22 15:04:07 -0600

    updates

--- lessons/lesson-10/js/owm-api-fetch-forecast.js
+++ lessons/lesson-10/js/owm-api-fetch-forecast.js
@@ -1,10 +1,7 @@
 fetch('https://api.openweathermap.org/data/2.5/forecast?&id=5604473&units=imperial&APPID=07407eccd051a7a7b4fc81e187f47771')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsObject) {
+    .then((response) => response.json())
+    .then((jsObject) => {
         console.log(jsObject);
-
         // go through the list of forecast values looking for 15:00 (3:00 p.m.) records and record index
         let tempforecast = [];
         let day = 1;
@@ -14,9 +11,8 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?&id=5604473&units=imperi
                 day++;
             }
         });
-        
-        for ( var i = 1; i <= 5; i++) {
+
+        for (let i = 1; i <= 5; i++) {
             document.getElementById('forecastday' + i).innerHTML = tempforecast[i];
         }
-
     });
--- lessons/lesson-10/js/owm-api-fetch.js
+++ lessons/lesson-10/js/owm-api-fetch.js
@@ -1,17 +1,13 @@
 fetch('https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperial&APPID=07407eccd051a7a7b4fc81e187f47771')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsObject) {
-        
+    .then((response) => response.json())
+    .then((jsObject) => {
         console.log(jsObject);
 
-        document.getElementById('current-temp').innerHTML = jsObject.main.temp;
-        document.getElementById('rain').innerHTML = jsObject.rain;
-        
-        let imagesrc = 'http://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-        let desc = jsObject.weather[0].description;
-        document.getElementById('weatherimage').innerHTML = imagesrc;
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        document.getElementById('imagesrc').textContent = imagesrc;
         document.getElementById('icon').setAttribute('src', imagesrc);
         document.getElementById('icon').setAttribute('alt', desc);
     });
